d52b87368fa33fcda4cb04da77f10099f5ea4fa2
BKoehring <curropt@yahoo.com>
2024-03-01 11:16:26 -0500

    Added a hot key to quit the game
    removed the ability to quit the game by pressing the x button and created a hot key combination to quit game

--- PlayerControl.gd
+++ PlayerControl.gd
@@ -1,7 +1,12 @@
 extends CharacterBody2D
 
-
+var ready_to_quit = false
 const SPEED = 300.0
+
+func _input(event):
+	if event.is_action_released("quit"):
+		ready_to_quit = true
+		get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
